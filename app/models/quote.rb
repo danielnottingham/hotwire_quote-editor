a6f5931a13630d3a3fe4eea008de40252e686a25
Daniel Nottingham <danielnottingham.ti@gmail.com>
2022-04-22 15:57:13 -0300

    Capitulo 11: Adicionando a soma total com turbo frames

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,15 +2,15 @@ class Quote < ApplicationRecord
   belongs_to :company
 
   has_many :line_item_dates, dependent: :destroy
+  has_many :line_items, through: :line_item_dates
 
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
 
-  # after_create_commit -> { broadcast_prepend_to 'quotes' }
-  # after_update_commit -> { broadcast_replace_to 'quotes' }
-  # after_destroy_commit -> { broadcast_remove_to 'quotes' }
-
-  # Those three callbacks are equivalent to the following single line
   broadcasts_to ->(quote) { [quote.company, 'quotes'] }, inserts_by: :prepend
+
+  def total_price
+    line_items.sum(&:total_price)
+  end
 end
